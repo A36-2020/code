5f030611324f11cc7119a0b0efb65b813c992c0f
Florina Sirghi <f.sirghi-1@student.tudelft.nl>
2020-02-18 22:57:24 +0100

    Complete version of centroid and MoI

--- Centroid.py
+++ Centroid.py
@@ -10,20 +10,23 @@ Area=[]
 angle = math.atan((0.5*h)/(Ca-0.5*h))
 stspace = (2*math.sqrt((0.5*h)**2+ (Ca-0.5*h)**2)+ math.pi*0.5*h)/11
 zcircfront = stspace*(0.5*h)/(0.5*math.pi*0.5*h)
-y_centroidcirc = math.sqrt((0.5*h)**2-((0.5*h)- zcircfront)**2)
+y_centroidcirc = math.sqrt((0.5*h)**2-((0.5*h)- zcircfront)**2) #this is for the stringers on the circular part
 z_centroidcirc = Ca-zcircfront
 
 z_centroidspar = Ca - 0.5*h
 y_centroidspar = 0
+
 z_centroidskinup = (Ca-0.5*h)/2
 y_centroidskinup = 0.25*h
+
 z_centroidskindown = (Ca-0.5*h)/2
 y_centroidskindown = -0.25*h
-z_centroidcirc = Ca-(2*0.5*h/math.pi)
-y_centroidcirc = 0
 
-y_centroid+=[y_centroidskinup, y_centroidcirc, y_centroidskindown, y_centroidspar]
-z_centroid+=[z_centroidskinup, z_centroidcirc, z_centroidskindown, z_centroidspar]
+z_centroid_semicirc = Ca - 0.5*h+ 4*0.5*h/(3*math.pi)
+y_centroid_semicirc = 0
+
+y_centroid+=[y_centroidskinup, y_centroid_semicirc, y_centroidskindown, y_centroidspar]
+z_centroid+=[z_centroidskinup, z_centroid_semicirc, z_centroidskindown, z_centroidspar]
       
 areaskin = math.sqrt((0.5*h)**2+(Ca-0.5*h)**2)*tsk
 areacirc = math.pi*0.5*h*tsk
@@ -68,6 +71,6 @@ areasum = sum(Area)
 zcentroidtotal = ztimesA/areasum
 ycentroidtotal = ytimesA/areasum
 
-#print(zcentroidtotal, ycentroidtotal)
-    
+if __name__ == '__main__':
+    print(zcentroidtotal, ycentroidtotal)
     
