7b756de963d9515603e8113166bfc7cd7d074c89
wiegerhelsdingen <wiegerhelsdingen@gmail.com>
2020-02-19 12:39:35 +0100

    Q integration

--- AerodynamicLoad.py
+++ AerodynamicLoad.py
@@ -7,7 +7,7 @@ from mpl_toolkits import mplot3d
 #-------------------------------
 Nz = 81
 Nx = 41
-Ca = 0.505    
+Ca = 0.505
 la = 1.611
 
 #array with different loads
@@ -50,7 +50,37 @@ for j in range (40):
 #---------------------------------------
 
 
+# Integrating aerodynamic load along z-axis and calculating center of pressure
+# simpson integration method
+def simpson(F,z,x):
+    F = -1*F
+    a = z[0]
+    b = z[-1]
+    nz = len(z)
+    nx = len(x)
+    CoP = []
+    Q = []
+    dz = abs(float((b-a)/2))
+    print(F.shape)
+    print(F[0,:])
+    for i in np.arange(nx):
+        q = []
+        cop = []
+        for j in np.arange(nz):
+            if j % 2 != 0:
+                q.append(4*F[j,i])
+                cop.append(4*(F[j,i]*z[j]))
+            elif j % 2 == 0:
+                q.append(2*F[j,i])
+                cop.append(2*(F[j,i]*z[j]))
+        int_f = (F[0,i]+sum(q)+F[-1,i])*dz/3
+        int_fz= (F[0,i]*a+sum(cop)+F[-1,i]*b)*dz/3
+        Q.append(int_f)
+        CoP.append(int_fz/int_f)
+    return Q,CoP
 
+
+#Interpolate aerodynamic load over x-axis with n points
 #output array sample:
 q = [1,2,5,4,2,3]
 qz = [0.2,0.2,0.4,0.6,0.2,0.5]
@@ -129,9 +159,3 @@ E = 1000
 I = 1.2
 
 def maccaulay_reactionforces_inx(r1x,r2x,r3x,x,qlist,spanlength,E,I):
-
-
-
-
-
-
