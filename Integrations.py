95f864d4c6b190c97d9d4219d2c61c8d3a9cd15e
wiegerhelsdingen <wiegerhelsdingen@gmail.com>
2020-02-19 10:49:19 +0100

    edit integration

--- Integrations.py
+++ Integrations.py
@@ -2,22 +2,29 @@ import numpy as np
 
 
 # simpson interpolation
-def simpson(f,z):
-    f = -f
+def simpson(F,z,x):
+    F = -1*F
     a = z[0]
     b = z[-1]
-    n = len(f)
-    q = []
+    nz = len(z)
+    nx = len(x)
     CoP = []
-    dx = abs(float((b-a)/2))
-    # print(dx)
-    for i in np.arange(n):
-        if i % 2 != 0:
-            q.append(4*f[i])
-            CoP.append(4*f[i]*z[i])
-        elif i % 2 == 0:
-            q.append(2*f[i])
-            CoP.append(2*f[i]*z[i])
-    int_f = (f[0]+sum(q)+f[-1])*dx/3
-    int_fz= (f[0]*a+sum(CoP)+f[-1]*b)*dx/3
-    return int_f, (int_fz/int_f)
+    Q = []
+    dz = abs(float((b-a)/2))
+    print(F.shape)
+    print(F[0,:])
+    for i in np.arange(nx):
+        q = []
+        cop = []
+        for j in np.arange(nz):
+            if j % 2 != 0:
+                q.append(4*F[j,i])
+                cop.append(4*(F[j,i]*z[j]))
+            elif j % 2 == 0:
+                q.append(2*F[j,i])
+                cop.append(2*(F[j,i]*z[j]))
+        int_f = (F[0,i]+sum(q)+F[-1,i])*dz/3
+        int_fz= (F[0,i]*a+sum(cop)+F[-1,i]*b)*dz/3
+        Q.append(int_f)
+        CoP.append(int_fz/int_f)
+    return Q,CoP
