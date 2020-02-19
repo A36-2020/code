14f52335008edfead5d7ef714de82dd40c39f6b8
Florina Sirghi <f.sirghi-1@student.tudelft.nl>
2020-02-19 11:04:43 +0100

    added cell stuff

--- Centroid.py
+++ Centroid.py
@@ -5,6 +5,13 @@ from F100 import *
 
 z_centroid= []
 y_centroid= []
+
+z_centroids_straight= []
+y_centroids_straight= []
+
+z_centroids_semicirc= []
+y_centroids_semicirc= []
+
 Area=[]
 
 angle = math.atan((0.5*h)/(Ca-0.5*h))
@@ -26,7 +33,12 @@ z_centroid_semicirc = 0.5*h - 2*0.5*h/math.pi
 y_centroid_semicirc = 0
 
 y_centroid+=[y_centroidskinup, y_centroid_semicirc, y_centroidskindown, y_centroidspar]
+y_centroids_straight+=[y_centroidskinup, y_centroidskindown, y_centroidspar]
+y_centroids_semicirc+=[y_centroid_semicirc, y_centroidspar]
+
 z_centroid+=[z_centroidskinup, z_centroid_semicirc, z_centroidskindown, z_centroidspar]
+z_centroids_straight+=[z_centroidskinup, z_centroidskindown, z_centroidspar]
+z_centroids_semicirc+=[z_centroid_semicirc, z_centroidspar]
       
 areaskin = math.sqrt((0.5*h)**2+(Ca-0.5*h)**2)*tsk
 areacirc = math.pi*0.5*h*tsk
@@ -37,23 +49,29 @@ Area+=[areaskin, areacirc, areaskin, areaspar]
 Area+= 11*[areast]
 
 
-def stringercentroids(y_centroid, z_centroid):
+def stringercentroids(y_centroid, z_centroid, y_centroids_straight, z_centroids_straight, y_centroids_semicirc, z_centroids_semicirc):
     for n in range(4):
         y_centroidpos = math.sin(angle)*(0.5*stspace+n*stspace)
         y_centroid.append(y_centroidpos)
+        y_centroids_straight.append(y_centroidpos)
         z_centroidstraight = Ca- math.cos(angle)*(0.5*stspace+(n)*stspace)
         z_centroid.append(z_centroidstraight)
+        z_centroids_straight.append(z_centroidstraight)
         
     z_centroid+=[z_centroidcirc, 0, z_centroidcirc]
+    z_centroids_semicirc+=[z_centroidcirc, 0, z_centroidcirc]
     y_centroid+=[y_centroidcirc, 0, -y_centroidcirc]
+    y_centroids_semicirc+=[y_centroidcirc, 0, -y_centroidcirc]
 
     for n in range(4):
         y_centroidneg = math.sin(angle)*(0.5*stspace+(3-n)*stspace)
         y_centroid.append(-y_centroidneg)
+        y_centroids_straight.append(-y_centroidneg)
         z_centroidstraight = Ca- math.cos(angle)*(0.5*stspace+(3-n)*stspace)
         z_centroid.append(z_centroidstraight)
+        z_centroids_straight.append(z_centroidstraight)
         
-    return y_centroid, z_centroid
+    return y_centroid, z_centroid, y_centroids_straight, z_centroids_straight, y_centroids_semicirc, z_centroids_semicirc
 
 #print(stringercentroids(y_centroid, z_centroid))
 #print(Area)
@@ -61,7 +79,7 @@ def stringercentroids(y_centroid, z_centroid):
 ztimesA = 0
 ytimesA = 0
 
-y_centroid, z_centroid = stringercentroids(y_centroid, z_centroid)
+y_centroid, z_centroid, y_centroids_straight, z_centroids_straight, y_centroids_semicirc, z_centroids_semicirc = stringercentroids(y_centroid, z_centroid, y_centroids_straight, z_centroids_straight, y_centroids_semicirc, z_centroids_semicirc)
 for i in range(len(Area)):
     ztimesA += z_centroid[i]*Area[i]
     ytimesA += y_centroid[i]*Area[i]
@@ -71,7 +89,12 @@ areasum = sum(Area)
 zcentroidtotal = ztimesA/areasum
 ycentroidtotal = ytimesA/areasum
 
+
 print(z_centroid)
 if __name__ == '__main__':
     print(zcentroidtotal, ycentroidtotal)
+    print(z_centroids_straight)
+    print(y_centroids_straight)
+    print(z_centroids_semicirc)
+    print(y_centroids_semicirc)
     
--- Moment_of_Inertia.py
+++ Moment_of_Inertia.py
@@ -27,8 +27,54 @@ def Moment_of_inertia():
 
     return Izz_total, Iyy_total
 
+def Moment_of_Inertia_triangle():
+    Izz_skin_t = 2*((tsk * (areaskin/tsk)**3*math.sin(angle)**2)/12 + areaskin*(y_centroids_straight[0])**2) #for total straight skin
+    Iyy_skin_t = 2 * ((tsk * (areaskin / tsk) ** 3 * math.cos(angle) ** 2) / 12 + areaskin * (z_centroids_straight[0]-0.5*h)**2)  # for total straight skin
+
+    Izz_spar_t = tsp * h ** 3 / 12 + areaspar * (y_centroids_straight[2]) ** 2
+    Iyy_spar_t = h * tsp ** 3 / 12 + areaspar * (z_centroids_straight[2] - 0.5*h) ** 2
+
+    Izz_total_t = Izz_skin_t + Izz_spar_t
+    Iyy_total_t = Iyy_skin_t + Iyy_spar_t
+
+    Izz_stringer_t = 0
+    Iyy_stringer_t = 0
+    for i in range(8):
+        Izz_stringer_t += areast * (y_centroids_straight[3 + i]) ** 2
+        Iyy_stringer_t += areast * (z_centroids_straight[3 + i] - 0.5*h) ** 2
+
+    Izz_total_t += Izz_stringer_t
+    Iyy_total_t += Iyy_stringer_t
+
+    return Izz_total_t, Iyy_total_t
+
+
+def Moment_of_Inertia_semicirc():
+    Izz_circ_c = (math.pi * (0.5 * h) ** 3 * tsk) / 2 + areacirc * (y_centroids_semicirc[0]) ** 2
+    Iyy_circ_c = (math.pi / 8 - 8 / (9 * math.pi)) * ((0.5 * h) ** 4 - (0.5 * h - tsk) ** 4) + areacirc * (
+                z_centroids_semicirc[0] - 0.5*h) ** 2
+
+    Izz_spar_c = tsp * h ** 3 / 12 + areaspar * (y_centroids_semicirc[1]) ** 2
+    Iyy_spar_c = h * tsp ** 3 / 12 + areaspar * (z_centroids_semicirc[1] - 0.5 * h) ** 2
+
+    Izz_total_c = Izz_circ_c + Izz_spar_c
+    Iyy_total_c = Iyy_spar_c + Iyy_circ_c
+
+    Izz_stringer_c = 0
+    Iyy_stringer_c = 0
+    for i in range(3):
+        Izz_stringer_c += areast * (y_centroids_straight[2 + i]) ** 2
+        Iyy_stringer_c += areast * (z_centroids_straight[2 + i] - 0.5 * h) ** 2
+
+    Izz_total_c += Izz_stringer_c
+    Iyy_total_c += Iyy_stringer_c
+
+    return Izz_total_c, Iyy_total_c
+
 if  __name__ == '__main__':
     print(Moment_of_inertia())
+    print(Moment_of_Inertia_triangle())
+    print(Moment_of_Inertia_semicirc())
 
 
 
