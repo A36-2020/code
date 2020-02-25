d8a01d6e79f80ee94c6cb7cbeb0a6f1468f8b215
Florina Sirghi <f.sirghi-1@student.tudelft.nl>
2020-02-26 00:16:03 +0100

    Added validation plotting for the deflection along the hingeline

--- Validation_plotting.py
+++ Validation_plotting.py
@@ -11,10 +11,14 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
+""" Importing the Excel file where all the validation data has been formatted and saved"""
+
 loc = ("Validation_data.xlsx")
 
 wb = xlrd.open_workbook(loc)
 sheet = wb.sheet_by_index(0)
+
+"""Saving the x, y and z coordinates of all the nodes"""
 
 X_nodes = np.array([float(sheet.cell_value(0, 1))])
 Y_nodes = np.array([float(sheet.cell_value(0, 2))])
@@ -26,6 +30,7 @@ for i in range(sheet.nrows-1):
     Z_nodes = np.append(Z_nodes, float(sheet.cell_value(i + 1, 3)))
 
 """Creating an array with all the x y z coordinates for each node, without deflection"""
+
 all_nodes = np.array([[X_nodes[0], Y_nodes[0], Z_nodes[0]]])
 
 for i in range(len(X_nodes)-1):
@@ -33,6 +38,7 @@ for i in range(len(X_nodes)-1):
 
 
 """Creating an array with all x y z coordinates for each node, including deflection"""
+
 sheet_deflection= wb.sheet_by_index(7)
 
 all_nodes_deflections = np.array([[X_nodes[0]+float(sheet_deflection.cell_value(1,2)), Y_nodes[0]+float(sheet_deflection.cell_value(1,3)), Z_nodes[0]+float(sheet_deflection.cell_value(1,4))]])
@@ -42,6 +48,7 @@ for i in range(len(X_nodes)-1):
 
 
 """Creating an array that stores the numbers of the nodes that generate each element"""
+
 sheet1= wb.sheet_by_index(1)
 
 all_elements = np.array([[int(sheet1.cell_value(1,1)), int(sheet1.cell_value(1,2)), int(sheet1.cell_value(1,3)), int(sheet1.cell_value(1,4))]])
@@ -51,6 +58,7 @@ for i in range(sheet1.nrows-2):
 
 
 """Creating an array that stores the coordinates of each of the 4 nodes that generate each element, without deflection"""
+
 all_elements_nodes_coord = np.array([[all_nodes[int(sheet1.cell_value(1,1))-1], all_nodes[int(sheet1.cell_value(1,2))-1], all_nodes[int(sheet1.cell_value(1,3))-1], all_nodes[int(sheet1.cell_value(1,4))-1]]])
 
 for i in range(sheet1.nrows-2):
@@ -61,6 +69,7 @@ for i in range(sheet1.nrows-2):
 
 
 """Creating an array that stores the coordinates of each of the 4 nodes that generate each element, with deflection"""
+
 all_elements_nodes_coord_deflect = np.array([[all_nodes_deflections[int(sheet1.cell_value(1,1))-1], all_nodes_deflections[int(sheet1.cell_value(1,2))-1], all_nodes_deflections[int(sheet1.cell_value(1,3))-1], all_nodes_deflections[int(sheet1.cell_value(1,4))-1]]])
 
 for i in range(sheet1.nrows-2):
@@ -70,7 +79,7 @@ for i in range(sheet1.nrows-2):
 
 
 
-"""Saving the number of the element with the highest stress"""
+"""Saving the number of the elements with the highest stress"""
 max_stress_spar= 3842
 max_stress_skin= 2391
 max_stress_overall= 2391
@@ -81,6 +90,7 @@ max_shear_overall= 2390
 
 
 """Saving all the Von Mises stresses for the elements"""
+
 sheet_stress= wb.sheet_by_index(6)
 von_misses_stresses= np.array([float(sheet_stress.cell_value(1,6))])
 for i in range(sheet_stress.nrows-2):
@@ -89,10 +99,13 @@ for i in range(sheet_stress.nrows-2):
 
 
 """Saving all the shear stresses for the elements"""
+
 shear_stresses= np.array([float(sheet_stress.cell_value(1,7))])
 for i in range(sheet_stress.nrows-2):
     shear_stresses= np.append(shear_stresses, [float(sheet_stress.cell_value(i+2,7))], axis=0 )
 
+
+"""Plotting all the nodes in 3D"""
 
 def plotting_all_nodes():
 
@@ -110,9 +123,12 @@ def plotting_all_nodes():
 #print(vtx)
 #print(all_elements_nodes_coord_deflect[0])
 
+"""Defining a function for normalisation"""
+
 def normal(x, x_min, x_max):
     return (x-x_min)/(x_max-x_min)
 
+"""Plotting the von misses stress distribution along the elements in 3D, by plotting each element as a separate polygon"""
 
 def plot_polygon_von_misses():
     ax = a3.Axes3D(pl.figure())
@@ -136,6 +152,7 @@ def plot_polygon_von_misses():
     ax.set(xlim=(0,3000), ylim=(-150,150), zlim=(-600, 200))
     pl.show()
 
+"""Plotting the shear stress distribution along the elements in 3D, by plotting each element as a separate polygon"""
 def plot_polygon_shear():
     fig = pl.figure()
     ax = a3.Axes3D(fig)
@@ -162,15 +179,17 @@ def plot_polygon_shear():
 
 #plot_polygon_von_misses()
 #plot_polygon_shear()
-#print(all_elements[len(all_elements)-1][1])
 
-#point_for_element = np.array([[sum(all_elements_nodes_coord[0][:][0])/4, sum(all_elements_nodes_coord[0][:][1])/4, sum(all_elements_nodes_coord[0][:][2])/4, sum(all_elements_nodes_coord[0][:][3])/4]])
+"""Saving an "average" point for each element -> the coordinates of the point that would be exactly in the middle of each rectangular element"""
+
 point_for_element= [(all_elements_nodes_coord[0][0]+all_elements_nodes_coord[0][1]+all_elements_nodes_coord[0][2]+all_elements_nodes_coord[0][3])/4]
 for i in range(len(all_elements_nodes_coord)-1):
     point_for_element.append((all_elements_nodes_coord[i+1][0]+all_elements_nodes_coord[i+1][1]+all_elements_nodes_coord[i+1][2]+all_elements_nodes_coord[i+1][3])/4)
 
 point_for_element=np.array(point_for_element)
 
+
+"""Plotting the von mises stress distribution for the cross-section at the position of the element with highest von mises stress"""
 
 def cross_section_max_von_misses(x_coord):
     fig = plt.gcf()
@@ -204,6 +223,7 @@ def cross_section_max_von_misses(x_coord):
     ax.set_xlabel('z (m)')
     plt.show()
 
+"""Plotting the shear stress distribution for the cross-section at the position of the element with highest shear stress"""
 
 def cross_section_max_shear_stress(x_coord):
     fig = plt.gcf()
@@ -237,9 +257,71 @@ def cross_section_max_shear_stress(x_coord):
     ax.set_xlabel('z (m)')
     plt.show()
 
-cross_section_max_von_misses(point_for_element[2390][0]) #the element with max von mises stress is 2391
+#cross_section_max_von_misses(point_for_element[2390][0]) #the element with max von mises stress is 2391
 
-cross_section_max_shear_stress(point_for_element[2389][0]) #the element with max shear stress is 2389
+#cross_section_max_shear_stress(point_for_element[2389][0]) #the element with max shear stress is 2389
 
+"""Plotting the deflection of the hingeline with respect to y"""
 
+def plotting_deflection_of_hingeline_y():
+    nodes_on_hingeline_y=[]
+    nodes_on_hingeline_x=[]
+    for i in range(len(all_nodes)):
+        if all_nodes[i][1]==0 and all_nodes[i][2]==0:
+            nodes_on_hingeline_y.append(all_nodes_deflections[i][1])
+            nodes_on_hingeline_x.append(all_nodes_deflections[i][0])
 
+    nodes_on_hingeline_y=np.array(nodes_on_hingeline_y)
+    nodes_on_hingeline_x = np.array(nodes_on_hingeline_x)
+
+    fig = plt.gcf()
+    ax = fig.gca()
+
+    for i in range(len(nodes_on_hingeline_x)):
+
+        plt.scatter(nodes_on_hingeline_x[i], nodes_on_hingeline_y[i], c="b", marker='o')
+
+    ax.set_ylabel('y (mm)')
+    ax.set_xlabel('x (mm)')
+    ax.set_title('Deflection of hinge line in y')
+
+    plt.show()
+
+"""Plotting the deflection of the hingeline with respect to z"""
+
+def plotting_deflection_of_hingeline_z():
+    nodes_on_hingeline_z = []
+    nodes_on_hingeline_x = []
+    for i in range(len(all_nodes)):
+        if all_nodes[i][1] == 0 and all_nodes[i][2] == 0:
+            nodes_on_hingeline_z.append(all_nodes_deflections[i][2])
+            nodes_on_hingeline_x.append(all_nodes_deflections[i][0])
+
+    nodes_on_hingeline_z = np.array(nodes_on_hingeline_z)
+    nodes_on_hingeline_x = np.array(nodes_on_hingeline_x)
+
+    fig = plt.gcf()
+    ax = fig.gca()
+
+    for i in range(len(nodes_on_hingeline_x)):
+        plt.scatter(nodes_on_hingeline_x[i], nodes_on_hingeline_z[i], c="b", marker='o')
+
+    ax.set_ylabel('z (mm)')
+    ax.set_xlabel('x (mm)')
+    ax.set_title('Deflection of hinge line in z')
+
+    plt.show()
+
+mini=1000
+c=100000
+for i in range(len(all_nodes)):
+
+    if int(all_nodes_deflections[i][1])==int(all_nodes_deflections[i][2]) and int(all_nodes_deflections[i][1])==0:
+        if all_nodes_deflections[i][1]<mini or all_nodes_deflections[i][2]<mini:
+            mini=min(all_nodes_deflections[i][1], all_nodes_deflections[i][2])
+            c=i
+
+#print(all_nodes_deflections[c], c)
+min_deflec=all_nodes[c]
+
+plotting_deflection_of_hingeline_y()
